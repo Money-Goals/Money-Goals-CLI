1146bf5f03537441294c2845897afa5b990377a1
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-12-12 12:41:35 -0800

    signed in user

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,25 @@
 const chalk = require("chalk");
 let prompt = require("prompt-sync")();
 
-console.log(chalk.bold.underline.bgWhite.cyan("Welcome to Money Goals!"));
+async function loadPrompts() {
+  console.log(chalk.bold.underline.bgWhite.cyan("Welcome to Money Goals!"));
 
-const email = prompt("Hello. What is your email? ");
+  const email = prompt("Hello. What is your email? ");
 
-console.log(chalk.bold.red(`Hello ${email}!`));
-
-const password = prompt.hide("What is your password? ");
-
-console.log(password);
+  console.log(chalk.bold.red(`Hello ${email}!`));
+  const password = prompt.hide("What is your password? ");
+  await loadUser(email, password);
+}
+async function loadUser(email, password) {
+  const resp = await fetch(`${process.env.API_URL}/api/v1/users/sessions`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+    credentials: "include",
+  });
+  const data = await resp.json();
+  console.log(data);
+}
