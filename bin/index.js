8b14f353e9f3dc89b91a1bb3d1f1ab09b8633642
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-12 13:31:57 -0600

    Install chalk and prompt-sync, add starting prompts

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
 
-console.log("Hello");
+const chalk = require("chalk");
+let prompt = require("prompt-sync")();
+// const inquirer = require("inquirer");
+
+// console.log(inquirer
+//   .prompt([
+//     "What is your monthly income?"
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   }));
+
+console.log(chalk.bold.underline.bgWhite.cyan("Welcome to Money Goals!"));
+
+const email = prompt("Hello. What is your email? ");
+
+console.log(chalk.bold.red(`Hello ${email}!`));
+
+const password = prompt.hide("What is your password? ");
+
+console.log(password);
