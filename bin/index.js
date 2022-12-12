6d4a3c72a3ef4a5c446afba7759a3704979488b8
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-12 13:52:38 -0600

    Remove inquirer code

--- bin/index.js
+++ bin/index.js
@@ -2,22 +2,6 @@
 
 const chalk = require("chalk");
 let prompt = require("prompt-sync")();
-// const inquirer = require("inquirer");
-
-// console.log(inquirer
-//   .prompt([
-//     "What is your monthly income?"
-//   ])
-//   .then((answers) => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   }));
 
 console.log(chalk.bold.underline.bgWhite.cyan("Welcome to Money Goals!"));
 
