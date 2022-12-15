de660c3433d3f9de8b6b8d2eb829a4b0a5c8d056
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-12-15 10:31:30 -0800

    inquirer additions for track choice

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ const chalk = require("chalk");
 let prompt = require("prompt-sync")();
 const fetch = require("cross-fetch");
 const cookie = require("cookie");
+const inquirer = require("inquirer");
 
 require("dotenv").config();
 
@@ -15,36 +16,81 @@ async function loadPrompts() {
 
   let validUser = false;
   let cookieInfo;
-  while(!validUser){
-      const email = prompt("Hello. What is your email? ");
-      console.log(chalk.bold.red(`Hello ${email}!`));
-      const password = prompt.hide("What is your password? ");
+  while (!validUser) {
+    const email = prompt("Hello. What is your email? ");
+    console.log(chalk.bold.red(`Hello ${email}!`));
+    const password = prompt.hide("What is your password? ");
     try {
-        cookieInfo = await signInUser(email, password);
-        validUser = true;
+      cookieInfo = await signInUser(email, password);
+      validUser = true;
     } catch (e) {
-        console.log(chalk.bold.red(e.message));
+      console.log(chalk.bold.red(e.message));
     }
   }
-  const secrets = await fetchAccounts(cookieInfo);
-  console.log(secrets);
 
   const monthlyIncome = prompt("What is your net income per month? ");
   const housing = prompt("How much do you spend on housing per month? ");
-  const transportation = prompt("How much do you spend on transportation per month? ");
+  const transportation = prompt(
+    "How much do you spend on transportation per month? "
+  );
   const groceries = prompt("How much do you spend on groceries per month? ");
-  const insurance = prompt("How much do you spend on insurance per month (including auto insurance, health insurance, life insurance, disability insurance?) ");
-  const healthcare = prompt("How much do you spend on additional healthcare costs per month? ");
+  const insurance = prompt(
+    "How much do you spend on insurance per month (including auto insurance, health insurance, life insurance, disability insurance?) "
+  );
+  const healthcare = prompt(
+    "How much do you spend on additional healthcare costs per month? "
+  );
   const utilities = prompt("How much do you spend on utilities per month? ");
-  const miscellaneous = prompt("How much do you spend on miscellaneous items (clothing, home furnishings, gym memberships, entertainment) per month? ");
-  const savings = prompt("How much do you spend on savings, investments, and debt payments per month? ");
-  
-  const userAccountInput = {monthlyIncome, housing, transportation, groceries, insurance, healthcare, utilities, miscellaneous, savings}
+  const miscellaneous = prompt(
+    "How much do you spend on miscellaneous items (clothing, home furnishings, gym memberships, entertainment) per month? "
+  );
+  const savings = prompt(
+    "How much do you spend on savings, investments, and debt payments per month? "
+  );
+
+  const userAccountInput = {
+    monthlyIncome,
+    housing,
+    transportation,
+    groceries,
+    insurance,
+    healthcare,
+    utilities,
+    miscellaneous,
+    savings,
+  };
 
   const accountInfo = await postAccounts(userAccountInput, cookieInfo);
-  const remaining = monthlyIncome - housing - transportation - groceries - insurance - healthcare - utilities - miscellaneous - savings
-  console.log('You have $', remaining, 'remaining each month');
-}
+  const remaining =
+    monthlyIncome -
+    housing -
+    transportation -
+    groceries -
+    insurance -
+    healthcare -
+    utilities -
+    miscellaneous -
+    savings;
+  console.log("You have $", remaining, "remaining each month");
 
+  //INQUIRER BREAK//
+  const questions = [
+    {
+      type: "list",
+      name: "track",
+      message: "Which financial track best fits your needs?",
+      choices: ["Debt", "Saving", "Invest"],
+      filter(val) {
+        return val.toLowerCase();
+      },
+    },
+  ];
+
+  inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, " "));
+  });
+  //INQUIRER BREAK//
+}
 loadPrompts();
+
 
